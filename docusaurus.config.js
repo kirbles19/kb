a659687b8a4f2f095c8d0875e4911a48a2bc8d43
Alex Fiedler <38628621+kirbles19@users.noreply.github.com>
2023-04-19 19:01:16 +0300

    fixes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://kirbles19.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/alpha-kb',
+  baseUrl: '/kb',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
